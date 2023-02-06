f04fba948ccc523fa6932bad5e4ca2fee20a749a
Tamilvel13 <124541420+Tamilvel13@users.noreply.github.com>
2023-02-06 07:33:22 +0530

    Add files via upload

---
+++ BrowserLaunch.java
@@ -0,0 +1,24 @@
+package org.login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class BrowserLaunch {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\hp\\eclipse-workspace\\SeleniumPractice\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+				driver.get("https://www.redbus.in/");
+				driver.manage().window().maximize();
+		
+				WebElement findElement = driver.findElement(By.id("src"));
+				findElement.sendKeys("thanjavur");
+				Select select = new Select (driver.findElement(By.id("src")));
+				select.selectByVisibleText("Old Bus Stand, Thanjavur");
+				
+				
+	}
+
+}
